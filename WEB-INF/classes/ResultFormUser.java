24a97796112ff254dca337bc329a48c16c93f857
Pierre DELGRANGE <pierredelgrange@hotmail.fr>
2018-12-09 17:42:18 +0100

    ajout navbar + css user

--- WEB-INF/classes/ResultFormUser.java
+++ WEB-INF/classes/ResultFormUser.java
@@ -8,8 +8,8 @@ import java.sql.*;
 public class ResultFormUser extends HttpServlet
 {
   public void service( HttpServletRequest req, HttpServletResponse res ) 
-       throws ServletException, IOException
-    {
+  throws ServletException, IOException
+  {
     PrintWriter out = res.getWriter();
 
     String nom = req.getParameter("nom");
@@ -30,42 +30,74 @@ public class ResultFormUser extends HttpServlet
     String preferenceAprem = req.getParameter("preferenceAprem");
 
     out.println("<!doctype html>");
+    out.println("<html lang=\"fr\">");
+    out.println("");
     out.println("<head>");
+    out.println("<meta charset=\"UTF-8\">");
     out.println("<link rel=\"stylesheet\" href=\"/projetWeb/style/resultform.css\"/>");
-    out.println("<title>Resultat du Formulaire</title>");
+    out.println("<title>R&eacute;sultat du Formulaire</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>Servlet ResultatFormulaire</h1> ");
-    out.println("<section>");
-    out.println("<br>");
-    out.println("Bonjour ");
-    out.println("<br>");
-    out.println("Nom = " + nom);
-    out.println("<br>");
-    out.println("Prenom = "+ prenom);
-    out.println("<br>");
-    out.println("email = " + email);
-    out.println("<br>");
-    out.println("telephone = " + telephone);
-    out.println("<br>");
-    out.println("preferenceRappel = " + preferenceRappel);
-    out.println("<br>");
-    out.println("preferenceLundi = " + preferenceLundi);
-    out.println("<br>");
-    out.println("preferenceMardi = "+ preferenceMardi);
-    out.println("<br>");
-    out.println("preferenceMercredi = " + preferenceMercredi);
-    out.println("<br>");
-    out.println("preferenceJeudi = " + preferenceJeudi);
-    out.println("<br>");
-    out.println("preferenceVendredi = " + preferenceVendredi);
-    out.println("<br>");
-    out.println("preferenceSamedi = " + preferenceSamedi);
-    out.println("<br>");
-    out.println("preferenceMatin = " + preferenceMatin);
-    out.println("<br>");
-    out.println("preferenceAprem = " + preferenceAprem);
-    out.println("<br>");
+    out.println("<header>");
+    out.println("<nav>");
+    out.println("<ul>");
+    out.println("<li><a href=\"http://localhost:8080/projetWeb/userindex.html\">Accueil</a></li>");
+    out.println("<li><a href=\"http://localhost:8080/projetWeb/servlet/Make\">Rendez-vous</a></li>");
+    out.println("<li><a href=\"http://localhost:8080/projetWeb/usercontact.html\">Contact</a></li>");
+    out.println("</ul>");
+    out.println("</nav>");
+    out.println("</header>");
+    out.println("<main>");
+    out.println("<div>");
+    out.println("<h1>Votre demande est prise en compte</h1> ");
+    out.println("<p>Merci de votre confiance</p>");
+    out.println("</div>");
+    out.println("<section class=\"info\">");
+    out.println("<h2>Nous avons enregistrer ces informations vous concernant</h2>");
+    out.println("<ul>");
+    out.println("<li>Nom : " + nom + "</li>");
+    out.println("<li>Pr&eacute;nom : "+ prenom + "</li>");
+    if(email != null){
+      out.println("<li>Email : " + email + "</li>");
+    }
+    if(telephone != null){
+      out.println("<li>Tel : " + telephone + "</li>");
+    }
+    if(preferenceRappel != null){
+      out.println("<li>Vous pr&eacute;f&eacute;rez &ecirc;tre rappel&eacute; par " + preferenceRappel + " afin de convenir d'un rendez-vous");
+    }
+     if(preferenceMatin != null){
+      out.println("en matin&eacute;e");
+    }
+    if(preferenceAprem != null){
+      out.println("<li>dans l'" + preferenceAprem);
+    }
+    out.println(" parmis les jours suivants :</li>");
+    out.println("</ul>");
+    out.println("<div class=\"jour\">");
+
+    if(preferenceLundi != null){
+      out.println("<p>" + preferenceLundi + "</p>");
+    }
+    if(preferenceMardi != null){
+      out.println("<p>"+ preferenceMardi + "</p>");
+    }
+    if(preferenceMercredi != null){
+      out.println("<p>" + preferenceMercredi + "</p>");
+    }
+    if(preferenceJeudi != null){
+      out.println("<p>" + preferenceJeudi + "</p>");
+    }
+    if(preferenceVendredi != null){
+      out.println("<p>" + preferenceVendredi + "</p>");
+    }
+    if(preferenceSamedi != null){
+      out.println("<p>" + preferenceSamedi + "</p>");
+    }
+    out.println("</div>");
+    out.println("</section>");
+    out.println("<section class=\"contact\">");
+    out.println("<h2>Nous vous recontactons au plus vite</h2>");
     out.println("</section>");
     try {
 // On déclare le type de driver JDBC et le chemin d’accès à la base, si pb exception ClassNotFound
@@ -102,21 +134,16 @@ public class ResultFormUser extends HttpServlet
         stat.close();
         conn.close();
       }
-    
+
     } catch (ClassNotFoundException ex) {
       ex.printStackTrace();
-    
+
     } catch (SQLException ex) {
       ex.printStackTrace();}
 
-      out.println("Ces informations sont ajoutées dans la base de donnée ...");
+      out.println("Ces informations sont ajout&eacute;es dans la base de donn&eacute;e ...");
       out.println("</body>");
       out.println("</html>");
 
-
-
-
-    
-    
+    }
   }
-}
